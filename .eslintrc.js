01d08838b99ed37fa6fb65a43262284403dbc21f
soichiro-nitta <soic1226@gmail.com>
2020-11-03 20:36:23 +0900

    eslint-config, prettier-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'prettier/@typescript-eslint',
-  ],
-  plugins: ['prettier', '@typescript-eslint'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
+  extends: '@soichiro_nitta/eslint-config/presets/typescript-prettier',
 }
