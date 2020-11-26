599df3cd0878fc73bce413b7cf9f3034b6b55bd7
soichiro-nitta <soic1226@gmail.com>
2020-11-27 00:29:31 +0900

    export modules

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,11 @@
-import set from './set'
 import to from './to'
-import x from './x'
-import y from './y'
-import width from './width'
-import height from './height'
-import color from './color'
-import scale from './scale'
-import scaleX from './scaleX'
-import rotation from './rotation'
-import opacity from './opacity'
-import strokeDasharray from './strokeDasharray'
-import strokeDashoffset from './strokeDashoffset'
-import strokeOpacity from './strokeOpacity'
-import fillOpacity from './fillOpacity'
-import boxShadow from './boxShadow'
-import backgroundColor from './backgroundColor'
+import set from './set'
+import repeat from './repeat'
+import delay from './delay'
 
 export const motion = {
-  set,
   to,
-  x,
-  y,
-  width,
-  height,
-  color,
-  scale,
-  scaleX,
-  rotation,
-  opacity,
-  strokeDasharray,
-  strokeDashoffset,
-  strokeOpacity,
-  fillOpacity,
-  boxShadow,
-  backgroundColor,
+  set,
+  repeat,
+  delay,
 }
