aa31ba0123152b56c1dbd52c36f4b4e582fc7238
soichiro-nitta <soic1226@gmail.com>
2020-11-27 00:29:05 +0900

    set

--- src/set.ts
+++ src/set.ts
@@ -1,10 +1,11 @@
-import { TweenMax } from 'gsap'
+import { assign } from './lib/assign'
+import { genStyleFromValues } from './lib/genStyleFromValues'
+import { ElementTypes, ValuesTypes } from './types'
 
-const set = (
-  element: Element | Element[] | HTMLCollection,
-  values: any
-): void => {
-  TweenMax.set(element, values)
+const set = (element: ElementTypes, values: ValuesTypes): void => {
+  const style = genStyleFromValues(values)
+
+  assign(element.style, style)
 }
 
 export default set
