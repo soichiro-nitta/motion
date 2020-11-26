6fae2da27d9ce4486e5a337bf8f7e6b479e95a59
soichiro-nitta <soic1226@gmail.com>
2020-11-27 00:29:20 +0900

    types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,20 @@
-export type HogeTypes = {
+import { Properties } from 'csstype'
+
+export type SpecificTypes = {
+  rotate?: string
+  rotateX?: string
+  rotateY?: string
+  rotateZ?: string
+  scale?: string | number
+  scaleX?: string | number
+  scaleY?: string | number
+  translateX?: string
+  translateY?: string
+  translateZ?: string
 }
+
+export type CssTypes = Properties
+
+export type ElementTypes = HTMLElement
+
+export type ValuesTypes = CssTypes & SpecificTypes
