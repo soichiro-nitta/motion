0b4a185914926aad48dd76aa458cb35779f4b6fe
soichiro-nitta <soic1226@gmail.com>
2020-11-27 13:25:41 +0900

    update assign

--- src/lib/assign.ts
+++ src/lib/assign.ts
@@ -1,6 +1,6 @@
 export const assign = (
-  obj1: Record<string, any>,
-  obj2: Record<string, any>
+  o1: Record<string, any>,
+  o2: Record<string, any>
 ): Record<string, any> => {
-  return Object.assign(obj1, obj2)
+  return Object.assign(o1, o2)
 }
