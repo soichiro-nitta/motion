c89dfd9108080d15b73db77db558ef921b035082
soichiro-nitta <soic1226@gmail.com>
2024-01-03 22:44:43 +0900

    Add addTypeByIds function to motion

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import removeWillChange from './removeWillChange'
 import repeat from './repeat'
 import set from './set'
 import to from './to'
+import { ElementTypes } from './types'
 import _useMotion from './useMotion'
 
 export const motion = {
@@ -18,5 +19,15 @@ export const motion = {
   set,
   to,
 }
-
 export const useMotion = _useMotion
+export const addTypeByIds = <T = string>() => {
+  return motion as Omit<typeof motion, 'to'> & {
+    to: (
+      ...args: [
+        T | ElementTypes,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ...(Parameters<typeof motion.to> extends [any, ...infer U] ? U : never)
+      ]
+    ) => void
+  }
+}
